ec9c8a47d4a0df20928824b174eda4761ed4b2ce
James Kelly <63963591+JimMKelly@users.noreply.github.com>
2020-05-15 16:31:13 +0200

    Initial commit

---
+++ projects/games/reversi.js
@@ -0,0 +1,458 @@
+var turns = 0;
+var playerTurn = 1;
+var playerChar = "";
+var p1Char = "1";
+var p2Char = "2";
+var availChar = "A";
+var winChar = "W";
+var winner = 0;
+var p1Tot = localStorage.getItem("rp1Score")
+var p2Tot = localStorage.getItem("rp2Score")
+var p1Count = 0;
+var p2Count = 0;
+var showPlayer = false;
+var showAvail = false;
+var compPlayer = false;
+var cntL = 0;
+var cntR = 0;
+var cntU = 0;
+var cntD = 0;
+var cntDRU = 0;
+var cntDRD = 0;
+var cntDLU = 0;
+var cntDLD = 0;
+var canFlipR = false;
+var canFlipL = false;
+var canFlipU = false;
+var canFlipD = false;
+var canFlipDRU = false;
+var canFlipDRD = false;
+var canFlipDLU = false;
+var canFlipDLD = false;
+var moves = 0;
+var compMoves = [{}];
+var board = [
+	[' ',' ',' ',' ',' ',' ',' ',' '],
+	[' ',' ',' ',' ',' ',' ',' ',' '],
+	[' ',' ',' ',' ',' ',' ',' ',' '],
+	[' ',' ',' ','1','2',' ',' ',' '],
+	[' ',' ',' ','2','1',' ',' ',' '],
+	[' ',' ',' ',' ',' ',' ',' ',' '],
+	[' ',' ',' ',' ',' ',' ',' ',' '],
+	[' ',' ',' ',' ',' ',' ',' ',' ']
+];
+
+
+
+checkCanMove();
+
+function showHint() {
+	showAvail = !showAvail;
+	
+	if(showAvail) {
+		document.getElementById("showHintBtn").innerHTML = "Hide available moves.";
+	} else {
+		document.getElementById("showHintBtn").innerHTML = "Show available moves.";
+	}
+	
+	updateBoard();
+	//console.log("Total: " + turns);
+}
+
+function playComp() {
+	compPlayer = true;
+	updateBoard();
+	//compPlayer = false;
+}
+
+function countPieces() {
+	p1Count = 0;
+	p2Count = 0;
+	for(let i = 0; i < board.length; i++) {
+		for(let j = 0; j < board[0].length; j++) {
+			if(board[j][i] == p1Char) {
+				p1Count++;
+			}else if(board[j][i] == p2Char) {
+				p2Count++;
+			}
+		}
+	}
+	document.getElementById("p1Total").innerHTML = "Player 1 has " + p1Count;
+	document.getElementById("p2Total").innerHTML = "Player 2 has " + p2Count;
+}
+
+function checkFlip(cellChoice) {
+	var w = board[0].length;
+	var x = cellChoice.x - 1;
+	var y = cellChoice.y - 1;
+	playerTurn == 1 ? playerChar = p1Char : playerChar = p2Char;
+	var otherChar = (playerChar == p1Char) ? p2Char : p1Char;
+	cntL = 0;
+	cntR = 0;
+	cntU = 0;
+	cntD = 0;
+	cntDRU = 0;
+	cntDRD = 0;
+	cntDLU = 0;
+	cntDLD = 0;
+	canFlipR = false;
+	canFlipL = false;
+	canFlipU = false;
+	canFlipD = false;
+	canFlipDRU = false;
+	canFlipDRD = false;
+	canFlipDLU = false;
+	canFlipDLD = false;
+
+	//Check right
+	for(let i = x; i < w -1; i++) {
+		if(board[y][i+1] == otherChar) {
+			cntR++;
+		}else if((i!=x) && (board[y][i+1] == playerChar)) {
+			canFlipR = true;
+		}else {
+			break;
+		}
+	}
+
+	//Check left
+	for(let i = x; i > 0; i--) {
+		if(board[y][i-1] == otherChar) {
+			cntL++;
+		}else if((i!=x) && (board[y][i-1] == playerChar)) {
+			canFlipL = true;
+		}else {
+			break;
+		}
+	}
+	
+	//Check down
+	for(let i = y; i < w-1; i++) {
+		if(board[i+1][x] == otherChar) {
+			cntD++;
+		}else if((i!=y) && (board[i+1][x] == playerChar)) {
+			canFlipD = true;
+		}else {
+			break;
+		}
+	}
+	
+	//Check up
+	for(let i = y; i > 0; i--) {
+		if(board[i-1][x] == otherChar) {
+			cntU++;
+		}else if((i!=y) && (board[i-1][x] == playerChar)) {
+			canFlipU = true;
+		} else {
+			break;
+		}
+	}
+	
+	//Check diag right up
+	for(let i = y; i > 0; i--) {
+		let diff = y - i + 1;
+		if(board[i-1][x+diff] == otherChar) {
+			cntDRU++;
+		}else if((i!=y) && (board[i-1][x+diff] == playerChar)) {
+			canFlipDRU = true;
+		} else {
+			break;
+		}
+	}
+	
+	//Check diag right down
+	for(let i = y; i < w-1; i++) {
+		let diff = i - y + 1;
+		if(board[i+1][x-diff] == otherChar) {
+			cntDRD++;
+		}else if((i!=y) && (board[i+1][x-diff] == playerChar)) {
+			canFlipDRD = true;
+		} else {
+			break;
+		}
+	}
+	
+	//Check diag left up
+	for(let i = y; i > 0; i--) {
+		let diff = y - i + 1;
+		if(board[i-1][x-diff] == otherChar) {
+			cntDLU++;
+		}else if((i!=y) && (board[i-1][x-diff] == playerChar)) {
+			canFlipDLU = true;
+		} else {
+			break;
+		}
+	}
+	
+	//Check diag left down
+	for(let i = y; i < w-1; i++) {
+		let diff = i - y + 1;
+		if(board[i+1][x+diff] == otherChar) {
+			cntDLD++;
+		}else if((i!=y) && (board[i+1][x+diff] == playerChar)) {
+			canFlipDLD = true;
+		} else {
+			break;
+		}
+	}
+}
+
+function flipPieces(cellChoice) {
+	var x = cellChoice.x - 1;
+	var y = cellChoice.y - 1;
+	//Flip right
+	if(cntR > 0 && canFlipR){
+		for(let n = x; n <= x + cntR; n++) {
+			board[y][n] = playerChar;
+		}
+	}
+
+	//Flip left
+	if(cntL > 0 && canFlipL){
+		for(let n = x - cntL; n <= x; n++) {
+			board[y][n] = playerChar;
+		}
+	}
+		
+	//Flip down
+	if(cntD > 0 && canFlipD){
+		for(let n = y; n <= y + cntD; n++) {
+			board[n][x] = playerChar;
+		}
+	}
+	
+	//Flip up
+	if(cntU > 0 && canFlipU){
+		for(let n = y - cntU; n <= y; n++) {
+			board[n][x] = playerChar;
+		}
+	}
+	
+	//Flip diag right up
+	if(cntDRU > 0 && canFlipDRU){
+		for(let n = y - cntDRU; n <= y; n++) {
+			let diff = y - n;
+			board[n][x+diff] = playerChar;
+		}
+	}
+	
+	//Flip diag right down
+	if(cntDRD > 0 && canFlipDRD){
+		for(let n = y; n <= y + cntDRD; n++) {
+			let diff = n - y;
+			board[n][x-diff] = playerChar;
+		}
+	}
+	
+	//Flip diag left up
+	if(cntDLU > 0 && canFlipDLU){
+		for(let n = y - cntDLU; n <= y; n++) {
+			let diff = y - n;
+			board[n][x-diff] = playerChar;
+		}
+	}
+
+	//Flip diag left down
+	if(cntDLD > 0 && canFlipDLD){
+		for(let n = y; n <= y + cntDLD; n++) {
+			let diff = n - y;
+			board[n][x+diff] = playerChar;
+		}	
+	}
+}
+
+function nextTurn(cellChoice) {
+	playerTurn == 1 ? playerChar = p1Char : playerChar = p2Char;
+	
+	getChoice(cellChoice);
+	flipPieces(cellChoice);
+	countPieces();
+	playerTurn = 3 - playerTurn;
+	var txt = "Player " + playerTurn + "'s turn!";
+	document.getElementById("overText").innerHTML = txt;
+	turns++;
+	checkCanMove();
+	checkWin();
+}
+
+function updateBoard() {
+	document.getElementById('p1Score').innerHTML = "Player 1: " + localStorage.getItem("rp1Score");
+	document.getElementById('p2Score').innerHTML = "Player 2: " + localStorage.getItem("rp2Score");
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	var w = board[0].length * 100;
+	var h = w;
+	ctx.canvas.width  = w;
+	ctx.canvas.height = h;
+	var cellChoice = { x: 0, y: 0};
+
+	if(compPlayer) {
+		checkCanMove();
+		var ranNum = Math.floor(Math.random() * moves);
+		cellChoice.x = compMoves[ranNum].x;
+		cellChoice.y = compMoves[ranNum].y;
+		if(isValid(cellChoice)) {
+			nextTurn(cellChoice);
+			drawBoard();
+		}
+	} else {
+		canvas.addEventListener('mousedown', onCanvasClick, false);
+		function onCanvasClick() {
+			var ev = event;
+			var x = ev.pageX - canvas.offsetLeft;
+			var y = ev.pageY - canvas.offsetTop;
+	
+			cellChoice.x = Math.floor(x/100) + 1;
+			cellChoice.y = Math.floor(y/100) + 1;
+			//console.log("x: " + cellChoice.x + " y: " + cellChoice.y);
+			if(cellChoice.x != 0) {
+				if(isValid(cellChoice)) {
+					nextTurn(cellChoice);
+					drawBoard();
+				}
+			} 
+		}
+	}	
+	drawBoard();
+}
+
+function checkCanMove() {
+	moves = 0;
+	compMoves = [{}];
+	compMoves.splice(0,1);
+	for(let i = 0; i < board.length; i++) {
+		for(let j = 0; j < board[0].length; j++) {
+			var cellCheck = {x:i+1, y:j+1};
+			checkFlip(cellCheck);
+			if((board[j][i] != p1Char) && (board[j][i] != p2Char)) {
+				board[j][i] = " ";
+			}
+			if(canFlipR || canFlipL || canFlipU || canFlipD || canFlipDRU || canFlipDRD || canFlipDLU || canFlipDLD) {
+				if((board[j][i] != p1Char) && (board[j][i] != p2Char)) {
+					moves++;
+					board[j][i] = availChar;
+					var compXY = { x: i + 1 , y: j + 1};
+					compMoves.push(compXY);
+				}
+			}
+		}
+	}
+	//console.log("Available moves: " + moves);
+}
+
+function checkWin() {
+	if(moves == 0){
+		if(p1Count > p2Count) {
+			p1Tot++;
+			document.getElementById("overText").innerHTML = "Player 1 wins " + p1Count + " to " + p2Count;
+			localStorage.setItem("rp1Score",p1Tot);
+			document.getElementById('p1Score').innerHTML = "Player 1: " + localStorage.getItem("rp1Score");
+			document.getElementById("startBtn").innerHTML = "Play again?";
+			document.getElementById("startBtn").style.display = "inline-block";
+		}else if(p2Count > p1Count) {
+			p2Tot++;
+			document.getElementById("overText").innerHTML = "Player 2 wins " + p2Count + " to " + p1Count;
+			localStorage.setItem("rp2Score",p2Tot);
+			document.getElementById('p2Score').innerHTML = "Player 2: " + localStorage.getItem("rp2Score");
+			document.getElementById("startBtn").innerHTML = "Play again?";
+			document.getElementById("startBtn").style.display = "inline-block";
+		} else {
+			document.getElementById("overText").innerHTML = "Draw! No-one wins!";
+			document.getElementById("startBtn").innerHTML = "Play again?";
+			document.getElementById("startBtn").style.display = "inline-block";
+		}
+	}
+}
+
+function isValid(cellChoice) {
+	var x = cellChoice.x - 1;
+	var y = cellChoice.y - 1;
+	var validMove = false;
+	checkFlip(cellChoice);
+	
+	if(canFlipR || canFlipL || canFlipU || canFlipD || canFlipDRU || canFlipDRD || canFlipDLU || canFlipDLD) {
+		validMove = true;
+	}
+	if(moves == 0){
+		checkWin();
+		return false;
+	}
+
+	if(board[y][x] == p1Char || board[y][x] == p2Char) {
+		document.getElementById("overText").innerHTML = "Player " + playerTurn + " that spot is already taken! <br> Choose somewhere else!";
+		return false;
+	}else if(!validMove) {
+		document.getElementById("overText").innerHTML = "Player " + playerTurn + " that is an invalid move! <br> Choose somewhere else!";
+		return false;
+	}else {
+		document.getElementById("overText").innerHTML = "Player " + playerTurn + "'s turn!";
+		return true;
+	}
+}
+
+function getChoice(cellChoice) {
+	var x = cellChoice.x - 1;
+	var y = cellChoice.y - 1;
+	
+	//Add choice to array
+	board[y][x] = playerChar;
+}
+
+function drawBoard() {
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+
+	ctx.font = "50px Arial";
+	let rows = board.length;
+	let cols = board[0].length;
+	let cellSize = 50;
+	for(let i= 0; i < rows; i++) {
+		for(let j= 0; j < cols; j++) {
+			var xPos = j*100 + cellSize;
+			var yPos = i*100 + cellSize;
+			//Draw grid
+			ctx.beginPath();
+			ctx.moveTo(xPos - cellSize,yPos - cellSize);
+			ctx.lineTo(xPos + cellSize,yPos - cellSize);
+			ctx.lineTo(xPos + cellSize,yPos + cellSize);
+			ctx.lineTo(xPos - cellSize,yPos + cellSize);
+			ctx.lineTo(xPos - cellSize,yPos - cellSize);
+			ctx.closePath();
+			ctx.fillStyle = "grey";
+			ctx.fill();
+			ctx.stroke();
+			
+			//Draw P1
+			if(board[i][j] == p1Char || (winner == 1 && board[i][j] == winChar)) {
+				ctx.fillStyle = "rgb(27, 143, 33)";
+				ctx.beginPath();
+				ctx.arc(xPos, yPos, 44, 0, 2*Math.PI);
+				ctx.fill();
+					
+			//Draw P2
+			} else if(board[i][j] == p2Char || (winner == 2 && board[i][j] == winChar)){
+				ctx.fillStyle = "rgb(217, 44, 60)";
+				ctx.beginPath();
+				ctx.arc(xPos, yPos, 44, 0, 2*Math.PI);
+				ctx.fill();
+			
+			//Draw available moves
+			} else if((board[i][j] == availChar) && showAvail){
+				ctx.fillStyle = "rgb(147, 150, 147)";
+				ctx.beginPath();
+				ctx.arc(xPos, yPos, 44, 0, 2*Math.PI);
+				ctx.fill();
+			}
+		}
+	}
+}
+
+function resetScores() {
+	localStorage.setItem("rp1Score",0);
+	localStorage.setItem("rp2Score",0);
+	replay();
+}
+
+function replay() {
+	window.location.reload();
+}
---
+++ projects/games/tictactoe.js
@@ -0,0 +1,313 @@
+var turns = 0;
+var playerTurn = 1;
+var playerChar = "";
+var p1Char = "1";
+var p2Char = "2";
+var winChar = "W";
+var winner = 0;
+var p1Tot = localStorage.getItem("tp1Score")
+var p2Tot = localStorage.getItem("tp2Score")
+var board = [
+		[' ',' ',' '],
+		[' ',' ',' '],
+		[' ',' ',' ']];
+
+function nextTurn(col) {
+	playerTurn == 1 ? playerChar = p1Char : playerChar = p2Char;
+	document.getElementById("startBtn").style.display = "none";
+	getChoice(col);
+	winner = checkWin();
+	updateBoard();	
+	if(winner) {
+		setTimeout(function() {
+			var winTxt = "Player " + winner + " wins!";
+			if(winner == 1) { 
+				p1Tot++;
+				localStorage.setItem("tp1Score",p1Tot);
+				document.getElementById('p1Score').innerHTML = "Player 1: " + localStorage.getItem("tp1Score");
+			}else {
+				p2Tot++;
+				localStorage.setItem("tp2Score",p2Tot);
+				document.getElementById('p2Score').innerHTML = "Player 2: " + localStorage.getItem("tp2Score");
+			}
+			document.getElementById("overText").innerHTML = winTxt;
+			document.getElementById("startBtn").innerHTML = "Play again?";
+			document.getElementById("startBtn").style.display = "inline-block";
+		},1000);		
+	} else {
+		if(turns >= 8) {
+			setTimeout(function() {
+				document.getElementById("overText").innerHTML = "DRAW! No-one wins!";
+				document.getElementById("startBtn").innerHTML = "Play again?";
+				document.getElementById("startBtn").style.display = "block";
+			},1000);
+		}
+		playerTurn = 3 - playerTurn;
+		var txt = "Player " + playerTurn + "'s turn!";
+		document.getElementById("overText").innerHTML = txt;
+		turns++;
+	}
+}
+
+function cellToRC(cell) {
+	var row = 0;
+	var col = 0;
+	switch(cell) {
+		case 1:
+			row = 0;
+			col = 0;
+			break;
+		case 2:
+			row = 0;
+			col = 1;
+			break;
+		case 3:
+			row = 0;
+			col = 2;
+			break;
+		case 4:
+			row = 1;
+			col = 0;
+			break;
+		case 5:
+			row = 1;
+			col = 1;
+			break;
+		case 6:
+			row = 1;
+			col = 2;
+			break;
+		case 7:
+			row = 2;
+			col = 0;
+			break;
+		case 8:
+			row = 2;
+			col = 1;
+			break;
+		case 9:
+			row = 2;
+			col = 2;
+			break;
+	}
+	/*
+	var row = Math.floor(cell / 3);
+	var col = Math.floor(cell % 3) - 1;
+	if(cell==3) {row = 0};
+	if(cell==6) {row = 1};
+	if(row == 3) {row = +2};
+	if(col == -1) {col = +2};
+*/
+	var rowCol = {
+		row: row,
+		col: col
+	}
+	return rowCol;
+}
+
+function isValid(cell) {
+	var rowCol = cellToRC(cell);
+	var row = rowCol.row;
+	var col = rowCol.col;
+	
+	//console.log("Cell: " + cell + " Row: " + row + " Col: " + col);
+	if(board[row][col] == p1Char || board[row][col] == p2Char) {
+		return false;
+	}else {
+		return true;
+	}
+}
+
+function getChoice(cell) {
+	var rowCol = cellToRC(cell);
+	var row = rowCol.row;
+	var col = rowCol.col;
+	playerTurn == 1 ? playerChar = p1Char : playerChar = p2Char;
+
+	//Add choice to array
+	if(board[row][col] == " ") {
+		board[row][col] = playerChar;
+	}
+}
+
+function updateBoard() {
+	document.getElementById('p1Score').innerHTML = "Player 1: " + localStorage.getItem("tp1Score");
+	document.getElementById('p2Score').innerHTML = "Player 2: " + localStorage.getItem("tp2Score");
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	var w = 400;
+	var h = 400;
+	var pad = 30;
+	ctx.canvas.width  = w + pad;
+	ctx.canvas.height = h + pad;
+	var s = w / 3;
+	canvas.addEventListener('mousedown', onCanvasClick, false);
+	function onCanvasClick() {
+		var ev = event;
+		var x = ev.pageX - canvas.offsetLeft;
+		var y = ev.pageY - canvas.offsetTop;
+		var cellChoice = 0;
+		//console.log(x + ", " + y);
+		if(x >= 0 && x < s) {
+			if(y >= 0 && y < s) {
+				cellChoice = 1;
+			} else if(y >= s && y < 2*s) {
+				cellChoice = 4;
+			} else if(y >= 2*s && y < 3*s) {
+				cellChoice = 7;
+			}
+		} else if(x >= s && x < 2*s) {
+			if(y >= 0 && y < s) {
+				cellChoice = 2;
+			} else if(y >= s && y < 2*s) {
+				cellChoice = 5;
+			} else if(y >= 2*s && y < 3*s) {
+				cellChoice = 8;
+			}
+		} else if(x >= 2*s && x < 3*s) {
+			if(y >= 0 && y < s) {
+				cellChoice = 3;
+			} else if(y >= s && y < 2*s) {
+				cellChoice = 6;
+			} else if(y >= 2*s && y < 3*s) {
+				cellChoice = 9;
+			}
+		} 
+
+		if(!cellChoice == 0) {
+			if(!isValid(cellChoice)) {
+				document.getElementById("overText").innerHTML = "That cell is not available, Player " + playerTurn + " try another.";
+			} else {
+				nextTurn(cellChoice);
+				canvas.removeEventListener('mousedown', onCanvasClick, false);
+			}
+		} 
+	}
+	drawBoard();
+}
+
+function drawBoard() {
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	var s = canvas.width / 3;
+	var pad = 30;
+
+	let rows = board.length;
+	let cols = board[0].length;
+	for(let i= 0; i < rows; i++) {
+		for(let j= 0; j < cols; j++) {
+			var p = s/2;
+			var xPos = j*s + p;
+			var yPos = i*s + p;
+						
+			ctx.font = "50px Arial";
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = "black";
+			ctx.fillStyle = "black";
+
+			ctx.beginPath();
+			//Draw squares
+			ctx.moveTo(xPos - p,yPos - p);
+			ctx.lineTo(xPos + p,yPos - p);
+			ctx.lineTo(xPos + p,yPos + p);
+			ctx.lineTo(xPos - p,yPos + p);
+			ctx.lineTo(xPos - p,yPos - p);
+			ctx.closePath();
+			ctx.fillStyle = "grey";
+			ctx.fill();
+			ctx.stroke();
+
+			if(board[i][j] == winChar) {
+				ctx.beginPath();
+				//Draw winning squares
+				ctx.moveTo(xPos - p,yPos - p);
+				ctx.lineTo(xPos + p,yPos - p);
+				ctx.lineTo(xPos + p,yPos + p);
+				ctx.lineTo(xPos - p,yPos + p);
+				ctx.lineTo(xPos - p,yPos - p);
+				ctx.closePath();
+				//ctx.fillStyle = "rgb(150, 144, 60)";
+				ctx.fillStyle = "pink";
+				ctx.fill();
+				ctx.stroke();
+			}
+			
+			if(board[i][j] == p1Char || (winner == 1 && board[i][j] == winChar)) {
+				//Draw O
+				ctx.fillStyle = "rgb(54, 125, 217)";
+				ctx.beginPath();
+				ctx.arc(xPos, yPos, 50, 0, 2*Math.PI);
+				ctx.closePath();
+				ctx.moveTo(xPos ,yPos);
+				ctx.arc(xPos, yPos, 30, 2*Math.PI, 0,true);
+				ctx.fill();
+			} else if(board[i][j] == p2Char || (winner == 2 && board[i][j] == winChar)){
+				//Draw X
+				ctx.strokeStyle = "rgb(217, 44, 60)";
+				ctx.lineWidth = 20;
+				ctx.beginPath();
+				ctx.moveTo(xPos - 45,yPos - 45);
+				ctx.lineTo(xPos + 45,yPos + 45);
+				ctx.moveTo(xPos + 45,yPos - 45);
+				ctx.lineTo(xPos - 45,yPos + 45);
+				ctx.closePath();
+				ctx.stroke();
+			}
+		}
+	}
+}
+
+function checkWin() {
+	var player = playerTurn;
+	let rows = board.length;
+	let cols = board[0].length;
+	if (turns > 3) {
+		for(let i= 0; i < rows; i++) {
+			for(let j= 0; j < cols; j++) {
+				if(board[i][j] == playerChar) {
+					if((board[0][j] == board[1][j]) && (board[1][j] == board[2][j])) {
+						//console.log("Player " + playerTurn + " wins horizontally!");
+						board[0][j] = winChar;
+						board[1][j] = winChar;
+						board[2][j] = winChar;
+						return player;
+					}
+					if((board[i][0] == board[i][1]) && (board[i][1] == board[i][2])) {
+						//console.log("Player " + playerTurn + " wins vertically!");
+						board[i][0] = winChar;
+						board[i][1] = winChar;
+						board[i][2] = winChar;
+						return player;
+					}
+					if((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) {
+						//console.log("Player " + playerTurn + " wins diagonally right!");
+						board[0][0] = winChar;
+						board[1][1] = winChar;
+						board[2][2] = winChar;
+						return player;
+					}
+				}
+				if((board[0][2] == playerChar) && (board[0][2] == board[1][1]) && (board[1][1] == board[2][0])) {
+					//console.log("Player " + playerTurn + " wins diagonally left!");
+					board[0][2] = winChar;
+					board[1][1] = winChar;
+					board[2][0] = winChar;
+					return player;
+				}
+			}
+		}
+	}
+	
+	return 0;
+}
+
+function replay() {
+	window.location.reload();
+}
+
+function resetScores() {
+	localStorage.setItem("tp1Score",0);
+	localStorage.setItem("tp2Score",0);
+	replay();
+
+}
